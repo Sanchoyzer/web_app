f86a5474be3cfabf25cead358f73dc4b6e1ba02f
Alex <sss0991@gmail.com>
2018-12-23 23:51:17 +1000

    Отправка сообщений

--- test_app/test_forms/models.py
+++ test_app/test_forms/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailLog(models.Model):
+    date = models.DateTimeField(verbose_name='Дата отправки', db_index=True, auto_now_add=True)
+    is_success = models.BooleanField(verbose_name="Успешно?", db_index=True, default=False)
--- test_app/test_forms/views.py
+++ test_app/test_forms/views.py
@@ -1,13 +1,18 @@
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth import views as auth_views
 from django.urls import reverse
-
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.core.mail import get_connection, send_mail, mail_admins, send_mass_mail
+
+from smtplib import SMTPException
+
 from .forms import UserRegistrationForm
+from .models import EmailLog
+
+from dotenv import dotenv_values as dtv
 
 
 class IndexView(TemplateView):
@@ -48,4 +53,39 @@ class UsersView(ListView):
     context_object_name = 'users'
     queryset = User.objects.filter(is_active=True).order_by('username')
     template_name = 'test_forms/users.html'
+
+
+def send_email(request):
+    connection = get_connection(
+        host=dtv().get('EMAIL_HOST'),
+        port=dtv().get('EMAIL_PORT'),
+        username=dtv().get('EMAIL_HOST_USER'),
+        password=dtv().get('EMAIL_HOST_PASSWORD'),
+        use_tls=bool(int(dtv().get('EMAIL_USE_SSL', '1')))
+    )
+
+    superusers_emails_queryset = User.objects.filter(is_superuser=True, email__isnull=False).values_list('email')
+    superusers_emails = list(email_item[0] for email_item in superusers_emails_queryset)
+    try:
+        num_sent = send_mail(
+            subject='Тема письма',
+            message='Тело',
+            from_email=dtv().get('EMAIL_HOST_USER'),
+            recipient_list=superusers_emails,
+            connection=connection
+        )
+    except SMTPException:
+        num_sent = 0
+
+    log = EmailLog()
+    log.is_success = (num_sent > 0)
+    log.save()
+    return redirect('forms-index')
+
+
+class EmailLogView(ListView):
+    model = EmailLog
+    context_object_name = 'logs'
+    queryset = EmailLog.objects.all().order_by('date')
+    template_name = 'test_forms/log.html'
 
