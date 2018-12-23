f3206119e9f5cd9e5b8103e9861b591adf2c1a50
Alex <sss0991@gmail.com>
2018-12-23 17:46:58 +1000

    Основная страница + функции логин и логаут юзера

--- test_app/test_forms/urls.py
+++ test_app/test_forms/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='forms-index'),
+    path('', views.IndexView.as_view(), name='forms-index'),
+    path('signup/', views.SignUp.as_view(), name='forms-signup'),
+    path('login/', views.LoginView.as_view(), name='forms-login'),
+    path('logout/', views.logout_view, name='forms-logout'),
 ]
--- test_app/test_forms/views.py
+++ test_app/test_forms/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth import views as auth_views
 
 
-def index(request):
-    return render(
-        request,
-        'test_forms/index.html'
-    )
+class IndexView(TemplateView):
+    template_name = 'test_forms/index.html'
+
+
+class LoginView(auth_views.LoginView):
+    template_name = 'registration/login.html'
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('forms-index')
+
+
+class SignUp(CreateView):
+    template_name = 'registration/signup.html'
+
+
+
