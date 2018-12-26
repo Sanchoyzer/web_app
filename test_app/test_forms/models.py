e677351a3ea116dd8ed906eb36fe65ea63c9b535
Alex <sss0991@gmail.com>
2018-12-26 22:46:08 +1000

    nano code style

--- test_app/test_forms/models.py
+++ test_app/test_forms/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class EmailLog(models.Model):
     date = models.DateTimeField(verbose_name='Дата отправки', db_index=True, auto_now_add=True)
-    is_success = models.BooleanField(verbose_name="Успешно?", db_index=True, default=False)
+    is_success = models.BooleanField(verbose_name='Успешно?', db_index=True, default=False)
